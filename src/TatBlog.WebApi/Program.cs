f615d79d5ff64d2e145d76694bf975149ec9be9a
unknown <maiquangdinh.it.work@gmail.com>
2023-03-29 20:34:53 +0700

    còn hai trang cuoi cung

--- src/TatBlog.WebApi/Program.cs
+++ src/TatBlog.WebApi/Program.cs
@@ -1,4 +1,7 @@
+using TatBlog.WebApi.Endpoints;
 using TatBlog.WebApi.Extensions;
+using TatBlog.WebApi.Mapsters;
+using TatBlog.WebApi.Validations;
 
 var builder = WebApplication.CreateBuilder(args); {
 
@@ -7,12 +10,17 @@ var builder = WebApplication.CreateBuilder(args); {
         .ConfigureCors()
         .ConfigureNLog()
         .ConfigureServices()
-        .ConfigureSwaggerOpenApi();
+        .ConfigureSwaggerOpenApi()
+        .ConfigureMapster()
+        .ConfigureFluentValidation();
 }
 var app = builder.Build(); {
     // Configure the HTTP request pipeline
     app.SetupRequestPipeline();
 
+
+    // Configure API endpoints
+    app.MapAuthorEndpoints();
     app.Run();
 }
 
